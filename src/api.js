9cb00b6b4c4e8076c4ed805173974808b40ad659
MuhammedMohsn <muhammedmohsen1111@gmail.com>
2025-06-25 12:41:35 +0300

    add workflow

--- src/api.js
+++ src/api.js
@@ -1,20 +1,22 @@
-import axios from 'axios'
-let URL = "https://fakestoreapi.com"
+import axios from "axios";
+let URL = "https://fakestoreapi.com";
 // Get Home Page Products (Hot Deals)
 export let getProducts = async () => {
-    try {
-        let response = await axios.get(`${URL}/products`);
-        let data = await response.data;
-        return data;
-    }
-    catch (err) { console.log(err) }
-}
+  try {
+    let response = await axios.get(`${URL}/products`);
+    let data = await response.data;
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 // Get Categories
-export let getCategories= async () => {
-    try {
-        let response = await axios.get(`${URL}/products/categories`);
-        let data = await response.data;
-        return data;
-    }
-    catch (err) { console.log(err) }
-}
+export let getCategories = async () => {
+  try {
+    let response = await axios.get(`${URL}/products/categories`);
+    let data = await response.data;
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
